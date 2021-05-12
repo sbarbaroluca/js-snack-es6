41568bb484d3a1b1d447cca444f98c40fc364771
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-12 16:50:24 +0200

    bici da vedere

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -1,3 +1,48 @@
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
 
+let bicidacorsa = [ {
+  nome: "Bici d'epoca",
+  peso: 15
+},
+{
+  nome: "Bici elettrica",
+  peso: 13
+},
+{
+  nome: "Bici da corsa",
+  peso: 7
+},
+{
+  nome: "Bici per bambini",
+  peso: 10
+},
+{
+  nome: "Bici da downhill",
+  peso: 12
+},
+];
+
+console.log(bicidacorsa);
+const root = document.getElementById("root");
+const element = document.getElementsByClassName("element")[0];
+const [bici] = bicidacorsa;
+const {peso} = bici;
+
+let pesoMinore = peso;
+let biciLeggera = {};
+
+for (let i = 0; i < bicidacorsa.length; i++) {
+  const {peso} = bicidacorsa[i];
+  if (peso < pesoMinore) {
+    posoMinore = peso;
+    biciLeggera = bicidacorsa[i];
+  }
+}
+
+
+//element.addEventListener("click", () => root.innerHTML = `
+
+//document.addEventListener("click", function(){
+  //document.getElementById("demo").innerHTML = "Hello World";
+//});
