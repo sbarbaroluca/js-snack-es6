b38f3f6ade3452c68f5a66ab7ec859c3ffb4ad0c
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-13 10:20:55 +0200

    fine

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -1,7 +1,7 @@
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
 
-let bicidacorsa = [ {
+const bicidacorsa = [ {
   nome: "Bici d'epoca",
   peso: 15
 },
@@ -23,26 +23,42 @@ let bicidacorsa = [ {
 },
 ];
 
-console.log(bicidacorsa);
-const root = document.getElementById("root");
-const element = document.getElementsByClassName("element")[0];
-const [bici] = bicidacorsa;
-const {peso} = bici;
+let biciLeggera = bicidacorsa[0];
 
-let pesoMinore = peso;
-let biciLeggera = {};
+for (var i = 1; i < bicidacorsa.length; i++) {
+  let biciCorrente = bicidacorsa[i];
 
-for (let i = 0; i < bicidacorsa.length; i++) {
-  const {peso} = bicidacorsa[i];
-  if (peso < pesoMinore) {
-    posoMinore = peso;
-    biciLeggera = bicidacorsa[i];
+  if (biciCorrente.peso < biciLeggera.peso) {
+    biciLeggera = biciCorrente;
   }
 }
 
+const container = document.getElementById("container");
+const { nome, peso } = biciLeggera;
 
+container.innerHTML = `
+    <h1>La bici più leggera</h1>
+    <ul>
+        <li>Nome: <strong>${nome}</strong></li>
+        <li>Peso: <strong>${peso}</strong></li>
+    </ul> `;      
+
+console.log(nome);
+console.log(peso);
+
+
+
+//const root = document.getElementById("root");
+//const element = document.getElementsByClassName("element")[0];
+//const [bici] = bicidacorsa;
+//const {peso} = bici;
+//let pesoMinore = peso;
+//let biciLeggera = {};
+//for (let i = 0; i < bicidacorsa.length; i++) {
+  //const {peso} = bicidacorsa[i];
+  //if (peso < pesoMinore) {
+    //posoMinore = peso;
+    //biciLeggera = bicidacorsa[i];
+  //}
+//}
 //element.addEventListener("click", () => root.innerHTML = `
-
-//document.addEventListener("click", function(){
-  //document.getElementById("demo").innerHTML = "Hello World";
-//});
